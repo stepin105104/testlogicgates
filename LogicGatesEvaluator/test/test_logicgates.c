68d63674f68413c1317ce2a82ae0019161fb99d3
Y Sushank <69004423+stepin105104@users.noreply.github.com>
2020-09-19 09:50:30 +0530

    Update test_logicgates.c

--- LogicGatesEvaluator/test/test_logicgates.c
+++ LogicGatesEvaluator/test/test_logicgates.c
@@ -1,8 +1,6 @@
-#include "unity.h"
+/*#include "unity.h"
 #include "logic_gates.h"
-/* Required by the unity test framework */
 void setUp(){}
-/* Required by the unity test framework */
 void tearDown(){}
 
 void test_not()
@@ -116,10 +114,9 @@ void test_xnor()
 }
 void test_main(void)
 {
-/* Initiate the Unity Test Framework */
+
   UNITY_BEGIN();
 
-/* Run Test functions */
   RUN_TEST(test_not);
   RUN_TEST(test_and);
   RUN_TEST(test_or);
@@ -128,6 +125,61 @@ void test_main(void)
   RUN_TEST(test_xor);
   RUN_TEST(test_xnor);
 
-  /* Close the Unity Test Framework */
+  
   UNITY_END();
 }
+
+*/
+
+
+
+
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <logic_gates.h>
+#define PROJECT_NAME    "LogicGatesEvaluator"
+
+/* Prototypes for all the test functions */
+void test_not(void);
+
+
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "not", test_not);
+
+            
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+ void test_not(void)
+{
+
+
+  CU_ASSERT(0 == not(1));
+
+  CU_ASSERT(1 == not(0));
+
+}
+
